ee97d8329930e50dd3178d89bc356ce7bbd98d0b
Eric Lindgren <nephilim.eric@gmail.com>
2017-11-29 17:09:49 -0500

    implemented traffic visualization for nodes and edges

--- public/d3app.js
+++ public/d3app.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const width = 640,
 	  height = 480,
 	  nodeBaseRadius = 16,
@@ -52,10 +54,10 @@ function svgInit() {
 // }
 
 
-
 function updateGraph (gameState) {
-	edgify(gameState.map)
+	makeEdges(gameState.map)
 	attachPOEs(gameState)
+	attachRoutes(gameState)
 	attachCoords(gameState.map)
 	const nodeMap = gameState.map
 	console.log('updateGraph')
@@ -63,7 +65,7 @@ function updateGraph (gameState) {
 	// console.log("state:", gameState)
 	console.log("nodeMap:", nodeMap)
 
-	console.log("ng data before", nodeGroups.select("circle").data())	
+	// console.log("ng data before", nodeGroups.select("circle").data())	
 
 	nodeGroups.data(nodeMap.nodes)
 		.select("circle") // select instead of selectAll auto binds to parent data
@@ -81,15 +83,14 @@ function updateGraph (gameState) {
 		 	}
 		 	return "white"
 		 })
-		// .classed("player-connected", d => d.connectedPlayers.length>0)
-		// .classed("traffic", d => d.traffic.length>0)
-
-
+		.classed("player-connected", d => d.connectedPlayers.length>0)
+		.classed("traffic", d => d.traffic.length>0)
 
 
 	link.data(nodeMap.edges)
+		.classed("traffic", d => {console.log("edge:",d.id,"traffic length:", d.traffic.length); return d.traffic.length>0})
 
-	console.log("ng data after", nodeGroups.data())	
+	// console.log("ng data after", nodeGroups.data())	
 
 	// // Since we are updating the data with new objects,
 	// // we need to point our simulation at the new objects 
@@ -97,13 +98,35 @@ function updateGraph (gameState) {
 	simulation.nodes(nodeMap.nodes)
 	simulation.force("link")
             .links(nodeMap.edges);
-
-	// // update node and edge traffic
-		 
-	// // update player POEs and ongoing connections
-
-	// // update module contents
 }
+
+// function pulsate(selection) {
+// 	recursive_transitions();
+
+// 	function recursive_transitions() {
+// 		console.log('pulsate data:',selection.data())
+// 	  if (selection.data()[0].traffic>0) {
+// 	    selection.transition()
+// 	        .duration(400)
+// 	        .attr("stroke-width", 2)
+// 	        .attr("r", 8)
+// 	        .ease('sin-in')
+// 	        .transition()
+// 	        .duration(800)
+// 	        .attr('stroke-width', 3)
+// 	        .attr("r", 12)
+// 	        .ease('bounce-in')
+// 	        .each("end", recursive_transitions);
+// 	    } else {
+// 	    // transition back to normal
+// 	    selection.transition()
+// 	        .duration(200)
+// 	        .attr("r", 8)
+// 	        .attr("stroke-width", 2)
+// 	        .attr("stroke-dasharray", "1, 0");
+// 	    } 
+// 	}
+// }
 
 function attachCoords(nodeMap) {
 	const data = nodeGroups.data()
@@ -132,21 +155,78 @@ function attachPOEs(gameState) {
 	// console.log("post attachPoes", gameState.map.nodes)
 }
 
-function edgify(nodeMap) {
+function getEdgeIn(edgeID, edgeList) {
+	for (let edge of edgeList) {
+		// console.log("getEdge is comparing",edge.id, "and", edgeID)
+		if (edge.id == edgeID)
+			return edge
+	}
+	return null 
+}
+
+function attachRoutes(gameState) {
+	for (let node of gameState.map.nodes) {
+		node.connectedPlayers = []
+		node.traffic = []
+	}
+
+	for (let edge of gameState.map.edges) {
+		edge.traffic = []
+	}
+
+	console.log("pre attachRoutes", gameState.map)
+
+	for (let playerID of Object.keys(gameState.routes)) {
+		const route = gameState.routes[playerID]
+		const player = gameState.players[playerID]
+
+		// iterate in reverse since routes are reverse ordered
+		for (let i = route.nodes.length-1; i > -1; i--) {
+			// // attach traffic to nodes
+			const thisNode = route.nodes[i]
+			gameState.map.nodes[thisNode.id].traffic.push(player)
+
+			//attach traffic to edges
+			// if we're in the middle of the route push traffic to connector
+			let thisEdgeID
+			if (i > 0) {
+				thisEdgeID = makeEdgeID(route.nodes[i].id, route.nodes[i-1].id)
+			} else {
+				// otherwise add traffic between last node and target
+				thisEdgeID = makeEdgeID(route.nodes[0].id, route.endpoint.id)
+			}
+			console.log("attachRoutes edgeID:", thisEdgeID)
+			getEdgeIn(thisEdgeID, gameState.map.edges).traffic.push(player)
+			// pulsate(d3.select(".edgeID-"+thisEdgeID))
+		}
+
+		// attach endpoints
+		gameState.map.nodes[route.endpoint.id].connectedPlayers.push(player)
+	}
+
+	console.log("post attachRoutes", gameState.map)
+}
+
+function makeEdgeID(id1, id2) {
+	let edgeID
+	if (id1 > id2)
+		edgeID = id1 + "e" + id2
+	else
+		edgeID = id2 + "e" + id1
+	return edgeID
+}
+
+function makeEdges(nodeMap) {
 
 	// console.log('edgifying', nodeMap)
-	seenEdges = {}
+	const seenEdges = {}
 	nodeMap.edges = []
 
 	for (let i in nodeMap.nodes){
 
 		i = parseInt(i)
 		for (let connectionID of nodeMap.nodes[i].connections) {
-			let edgeID = ""
-			if (i > connectionID)
-				edgeID = i + "e" + connectionID
-			else
-				edgeID = connectionID + "e" + i
+			let edgeID = makeEdgeID(i, connectionID)
 
 			if (!seenEdges[edgeID])
 				seenEdges[edgeID] = {id:edgeID, source:i, target:connectionID}
@@ -156,7 +236,7 @@ function edgify(nodeMap) {
 	for (let edgeID of Object.keys(seenEdges)) {
 		nodeMap.edges.push(seenEdges[edgeID])
 	}
-	// console.log("edgify produced:",nodeMap)
+	// console.log("makeEdges produced:",nodeMap)
 
 
 }
@@ -167,7 +247,7 @@ function edgify(nodeMap) {
 function initGraph (nodeMap) {
 		console.log('Initializing Graph...');
 
-		edgify(nodeMap)
+		makeEdges(nodeMap)
 
 		simulation = d3.forceSimulation()
             .force("link", d3.forceLink().distance(nodeBaseRadius*4))
@@ -180,7 +260,7 @@ function initGraph (nodeMap) {
         	.data(nodeMap.edges)
             .enter()
             .append("line")
-            .attr("class", "edge")
+            .attr("class", d => "edge edgeID-" + d.id)
             .attr("stroke", "black")
             .attr("stroke-width", strokeWidth)
 
