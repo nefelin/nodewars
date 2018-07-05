e9d19adae1126b3779de08a263e3ed6e8df64f9c
Eric Lindgren <nephilim.eric@gmail.com>
2018-07-04 19:21:58 -0700

    fixed hardcoded localhost in websocket creation

--- fe_overhaul/src/stories/components/Protocol/NWSocket.js
+++ fe_overhaul/src/stories/components/Protocol/NWSocket.js
@@ -13,7 +13,7 @@ class NWSocket {
 
 		// const ws = new WebSocket(ws_protocol + window.location.host + '/ws');
 		parser.open()
-		this.ws = new WebSocket(ws_protocol + 'localhost:8080' + '/ws');
+		this.ws = new WebSocket(ws_protocol + window.location + '/ws');
 
 		this.ws.onerror = (e) => parser.error()
 
